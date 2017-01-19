5decbe74751ceeca27da21d400b2fd3ec00ff648
akhil1608 <ak160896@gmail.com>
2017-01-19 22:18:22 +0530

    Update somescript.js

--- js/somescript.js
+++ js/somescript.js
@@ -1,15 +1,24 @@
 function f(n){
 	document.getElementById("tab"+n).style.display="block";
 	document.getElementById("tab"+(n+1)%2).style.display="none";
+	document.getElementById("cat"+n).style.background="#f4a460";
+	document.getElementById("cat"+(n+1)%2).style.background="#ffffff";
 }
 function ft(n){
 	document.getElementById("tab"+n).style.display="block";
 	document.getElementById("tab"+(n+1)%4).style.display="none";
 	document.getElementById("tab"+(n+2)%4).style.display="none";
 	document.getElementById("tab"+(n+3)%4).style.display="none";
+	document.getElementById("cat"+n).style.background="#f4a460";
+	document.getElementById("cat"+(n+1)%4).style.background="#ffffff";
+	document.getElementById("cat"+(n+2)%4).style.background="#ffffff";
+	document.getElementById("cat"+(n+3)%4).style.background="#ffffff";
 }
 function fm(n){
 	document.getElementById("tab"+n).style.display="block";
 	document.getElementById("tab"+(n+1)%3).style.display="none";
 	document.getElementById("tab"+(n+2)%3).style.display="none";
+	document.getElementById("cat"+n).style.background="#f4a460";
+	document.getElementById("cat"+(n+1)%3).style.background="#ffffff";
+	document.getElementById("cat"+(n+2)%3).style.background="#ffffff";
 }
